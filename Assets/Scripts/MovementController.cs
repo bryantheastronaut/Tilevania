1ab3dcdb84c9c9916b0f61a0ec2e2114204455f1
bryan gilbraith <gilbraith.bryan@gmail.com>
2020-08-05 20:20:27 -0700

    adding player movement with input system

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider))]
+
+public class MovementController : MonoBehaviour {
+    [SerializeField] float runSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 wasdInput;
+    private float xMovement;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // OnMovement needs to be called `On` whatever the InputAsset Name is.
+    private void OnMovement(InputValue value) {
+        wasdInput = value.Get<Vector2>();
+        xMovement = wasdInput.x * runSpeed;
+    }
+
+    private void FixedUpdate() {
+        if (xMovement != 0) {
+            rb.velocity = new Vector2(xMovement, rb.velocity.y);
+        }
+    }
+}
