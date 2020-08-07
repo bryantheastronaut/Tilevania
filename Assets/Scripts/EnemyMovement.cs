b5ca2e724559b81c091eb9a6e446cca28f328a9b
bryan gilbraith <gilbraith.bryan@gmail.com>
2020-08-06 22:18:56 -0700

    adding enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    [SerializeField] float moveSpeed = 1f;
+    private Rigidbody2D rb;
+
+    private BoxCollider2D[] edgeColliders;
+     
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(moveSpeed, 0);
+        edgeColliders = GetComponents<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var currentMovement = rb.velocity;
+        foreach (BoxCollider2D collider in edgeColliders) {
+            if (!collider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+                currentMovement.x = currentMovement.x * -1;
+                break;
+            }
+        }
+        rb.velocity = currentMovement;
+        transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1);
+    }
+}
