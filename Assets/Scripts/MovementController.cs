671163c62eabc80e47ef7da12a1be71223653110
bryan gilbraith <gilbraith.bryan@gmail.com>
2020-08-06 18:42:10 -0700

    adding climbing

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -2,31 +2,39 @@
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Collider2D))]
 
 public class MovementController : MonoBehaviour {
     // Config
     [SerializeField] float runSpeed = 5f;
-    [SerializeField] float jumpPower = 15f;
+    [SerializeField] float jumpPower = 22.5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // Cached refs
     [SerializeField] InputActionAsset asset;
     private Rigidbody2D rb;
     private Animator anim;
+    private Collider2D col;
+
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
     }
 
     private void FixedUpdate() {
         CheckIsRunning();
-
-        bool didTriggerJump = asset.FindAction("Jump").triggered;
-        if (didTriggerJump) { Jump(); }
+        CanClimbLadder();
+        Jump();
+        
     }
 
     private void Jump() {
+        bool didTriggerJump = asset.FindAction("Jump").triggered;
+        bool canJump = col.IsTouchingLayers(LayerMask.GetMask("Ground")) || col.IsTouchingLayers(LayerMask.GetMask("Climbable"));
+        if (!canJump || !didTriggerJump) { return; }
+
         Vector2 jumpVelocityToAdd = new Vector2(0f, jumpPower);
         rb.velocity += jumpVelocityToAdd;
     }
@@ -45,5 +53,24 @@ public class MovementController : MonoBehaviour {
             // if player is moving horizontally, we need to reverse current scaling of x axis
             transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
         }
+    }
+
+    private void CanClimbLadder() {
+
+        if (!col.IsTouchingLayers(LayerMask.GetMask("Climbable"))) {
+            Debug.Log("Not climbable!");
+            anim.SetBool("isClimbing", false);
+            rb.gravityScale = 1f;
+            return;
+        }
+        Debug.Log("Climbable!");
+
+        rb.gravityScale = 0f;
+
+        var y = asset.FindAction("Movement").ReadValue<Vector2>().y * climbSpeed;
+        bool isMovingOnY = Mathf.Abs(y) > Mathf.Epsilon;
+
+        anim.SetBool("isClimbing", isMovingOnY);
+        rb.velocity = new Vector2(rb.velocity.x, isMovingOnY ? y : 0);
     }
 }
