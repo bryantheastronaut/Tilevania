a118256cd6575a25331c8726cb0182d4ab2cecf1
bryan gilbraith <gilbraith.bryan@gmail.com>
2020-08-06 22:28:12 -0700

    refactoring enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,25 +7,29 @@ public class EnemyMovement : MonoBehaviour {
     [SerializeField] float moveSpeed = 1f;
     private Rigidbody2D rb;
 
-    private BoxCollider2D[] edgeColliders;
+    private BoxCollider2D enemyFrontCollider;
      
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(moveSpeed, 0);
-        edgeColliders = GetComponents<BoxCollider2D>();
+        enemyFrontCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update() {
-        var currentMovement = rb.velocity;
-        foreach (BoxCollider2D collider in edgeColliders) {
-            if (!collider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
-                currentMovement.x = currentMovement.x * -1;
-                break;
-            }
+        if (IsFacingRight()) {
+            rb.velocity = new Vector2(moveSpeed, 0);
+        } else {
+            rb.velocity = new Vector2(-moveSpeed, 0);
         }
-        rb.velocity = currentMovement;
-        transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1);
+    }
+
+    private bool IsFacingRight() {
+        return Mathf.Sign(transform.localScale.x) > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        transform.localScale = new Vector2(-Mathf.Sign(rb.velocity.x), 1);
     }
 }
